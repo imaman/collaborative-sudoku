e52313fd5ed2784f321ed3bf8f0586a867951051
Itay Maman <imaman@google.com>
2014-06-18 11:02:31 +0300

    introduce start controller

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
 
 (function() {
-  var CLIENT_ID = '498879331099-gs0q4drp0b7l098iu0pigk2jrlthl7bq.apps.googleusercontent.com';
-  var FILE_ID = '0B7w8hD8aAtTHSVFGaDkzanpkN28';
+  //var CLIENT_ID = '498879331099-gs0q4drp0b7l098iu0pigk2jrlthl7bq.apps.googleusercontent.com';
+  // var FILE_ID = '0B7w8hD8aAtTHSVFGaDkzanpkN28';
 
   $(document).ready(function() {
+    startController();
+  });
+
+  function startController() {
     var controller = new Controller(new Board(), []);
     controller.flush();
     $('#clearButton').click(function() { controller.clear(); controller.flush(); });
     $('#undoButton').click(function() { controller.undo() });
     $('#redoButton').click(function() { controller.redo() });
-  });
+  }
 })();
