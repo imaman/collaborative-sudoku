05b340f80e7330a06f0b78d4d88d6e4b272151e0
Itay Maman <imaman@google.com>
2014-06-18 09:46:23 +0300

    pull rendering into view.js

--- index.js
+++ index.js
@@ -6,9 +6,12 @@
   }
 
   function Model(cellById, moves, doc) {
-    console.log('cellById=' + JSON.stringify(cellById));
     this.toString = function() {
       return JSON.stringify(cellById, null, '  ');
+    }
+
+    this.idFromPos = function(r,c) {
+      return idFromPos(r, c);
     }
 
     this.at = function(id) {
@@ -36,9 +39,6 @@
       var effectiveZone = all.filter(function(o) {
         return o.r === cell.r || o.c === cell.c || o.z === cell.z;
       });
-      if (effectiveZone.length != 21)
-        throw new Error('unexpected zone size. len=' + effectiveZone.length);
-
       var conflicting = effectiveZone.filter(function(o) {
         return (o.v === cell.v) && (cell.v != '');
       });
@@ -46,7 +46,7 @@
       return conflicting.length === 1;
     };
 
-    this.changeSelectionTo = function(v) {
+    this.makeMove = function(v) {
       if (!this.selected)
         return;
 
@@ -108,60 +108,6 @@
     return cellById;
   }
 
-  function renderCell(m, r, c) {
-    var id = idFromPos(r, c);
-    var cell = m.at(id);
-    var td = $('<input></input>', {id: cell.id, type: 'text', maxlength: 1, readonly: 'readonly'});
-
-    m.ok(id) || td.addClass('bad');
-    m.isSelected(id) && td.css('background-color', 'lightgrey');
-
-    td.addClass('scell');
-    if (r % 3 === 0)
-      td.addClass('btop');
-    if (r % 3 === 2)
-      td.addClass('bbot');
-    if (c % 3 === 0)
-      td.addClass('bleft');
-    if (c % 3 === 2)
-      td.addClass('bright');
-    td.click(function() {
-      m.select(this.id);
-    });
-    td.keyup(function(event) {
-    });
-
-    td.val(cell.v);
-    return td;
-  }
-
-  function render(m) {
-    var h = $('<div></div>');
-    m.getLast(10).forEach(function(curr) {
-      var item = $('<div></div>');
-      item.text(curr.displayName + ' ' + moment(curr.at).fromNow());
-//      if (curr.collaborator)
-//        item.css('color', curr.collaborator.color);
-      h.append(item);
-    });
-    $('#history').html(h);
-    var t = $('<div></div>');
-    t.addClass('sudoku');
-    for (var r = 0; r < 9; ++r) {
-      for (var c = 0; c < 9; ++c) {
-        t.append(renderCell(m, r, c));
-      }
-    }
-    t.keyup(function(event) {
-      if (event.which < 49 || event.which > 57)
-        return;
-
-      var n = event.which - 48;
-      m.changeSelectionTo(n);
-      console.log('keyup on table: ' + event.which);
-    });
-    $('#game').html(t);
-  }
 
   var CLIENT_ID = '498879331099-gs0q4drp0b7l098iu0pigk2jrlthl7bq.apps.googleusercontent.com';
   var FILE_ID = '0B7w8hD8aAtTHSVFGaDkzanpkN28';
---
+++ view.js
@@ -0,0 +1,53 @@
+function renderCell(m, r, c) {
+  var id = m.idFromPos(r, c);
+  var cell = m.at(id);
+  var td = $('<input></input>', {id: cell.id, type: 'text', maxlength: 1, readonly: 'readonly'});
+
+  m.ok(id) || td.addClass('bad');
+  m.isSelected(id) && td.css('background-color', 'lightgrey');
+
+  td.addClass('scell');
+  if (r % 3 === 0)
+    td.addClass('btop');
+  if (r % 3 === 2)
+    td.addClass('bbot');
+  if (c % 3 === 0)
+    td.addClass('bleft');
+  if (c % 3 === 2)
+    td.addClass('bright');
+  td.click(function() {
+    m.select(this.id);
+  });
+  td.keyup(function(event) {
+  });
+
+  td.val(cell.v);
+  return td;
+}
+
+function render(m) {
+  var h = $('<div></div>');
+  m.getLast(10).forEach(function(curr) {
+    var item = $('<div></div>');
+    item.text(curr.displayName + ' ' + moment(curr.at).fromNow());
+    h.append(item);
+  });
+  $('#history').html(h);
+  var t = $('<div></div>');
+  t.addClass('sudoku');
+  for (var r = 0; r < 9; ++r) {
+    for (var c = 0; c < 9; ++c) {
+      t.append(renderCell(m, r, c));
+    }
+  }
+  t.keyup(function(event) {
+    if (event.which < 49 || event.which > 57)
+      return;
+
+    var n = event.which - 48;
+    m.makeMove(n);
+    console.log('keyup on table: ' + event.which);
+  });
+  $('#game').html(t);
+}
+
