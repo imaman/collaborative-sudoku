8f4aa0e37cde41e23d80a572e65486160a9b9145
Itay Maman <imaman@google.com>
2014-06-18 10:35:34 +0300

    introduce board

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 
 
 (function() {
-  var cellById = {};
-  resetBoard(cellById);
+  function Board() {
+    var cellById = {};
 
-  function resetBoard(board) {
-    var i, j;
-    for (i = 0; i < 81; ++i) {
-      var r = Math.floor(i / 9);
-      var c = i % 9;
-      var id = 'cell_' + i;
-      board[id] = {id: id, r: r, c: c, z: Math.floor(r / 3) * 3 + Math.floor(c / 3), v: ''};
+    this.resetBoard = function() {
+      board = cellById;
+      var i, j;
+      for (i = 0; i < 81; ++i) {
+        var r = Math.floor(i / 9);
+        var c = i % 9;
+        var id = 'cell_' + i;
+        board[id] = {id: id, r: r, c: c, z: Math.floor(r / 3) * 3 + Math.floor(c / 3), v: ''};
+      }
     }
+
+    this.getCell = function(id) {
+      var res = cellById[id];
+      if (res)
+        return res;
+      throw new Error('No cell found for ' + id + ' ' + JSON.stringify(cellById));
+    }
+
+    this.getAllCells = function() {
+      var result = [];
+      var r, c;
+      for (r = 0; r < 9; ++r)
+        for (c = 0; c < 9; ++c)
+          result.push(this.getCell(idFromPos(r, c)));
+      return result;
+    }
+    this.resetBoard();
   }
+
 
   function idFromPos(r, c) {
     return 'cell_' + (r * 9 + c);
   }
 
-  function getCell(id) {
-    var res = cellById[id];
-    if (res)
-      return res;
-    throw new Error('No cell found for ' + id + ' ' + JSON.stringify(cellById));
-  }
-
-  function getAllCells() {
-    var result = [];
-    var r, c;
-    for (r = 0; r < 9; ++r)
-      for (c = 0; c < 9; ++c)
-        result.push(getCell(idFromPos(r, c)));
-    return result;
-  }
 
   function Controller(moves) {
+    var board = new Board();
     this.idFromPos = idFromPos;
 
     this.at = function(id) {
-      return getCell(id);
+      return board.getCell(id);
     }
 
     this.select = function(id) {
@@ -54,8 +60,8 @@
     }
 
     this.isValid = function(id) {
-      var cell = getCell(id);
-      var all = getAllCells();
+      var cell = board.getCell(id);
+      var all = board.getAllCells();
       var effectiveZone = all.filter(function(o) {
         return o.r === cell.r || o.c === cell.c || o.z === cell.z;
       });
@@ -79,9 +85,9 @@
     }
 
     this.flush = function() {
-      resetBoard(cellById);
+      board.resetBoard();
       moves.asArray().forEach(function(curr) {
-        getCell(curr.id).v = curr.v;
+        board.getCell(curr.id).v = curr.v;
       });
       render(this);
     }
